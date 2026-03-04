b847ea022d92f79fba7727c2983226d2286be7bb
Barkavi.E <barkavi.e@hogist.com>
2026-03-04 18:17:54 +0530

    added chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import MainRoutes from './routes/MainRoutes';
 import { ThemeProvider } from '@mui/material';
 import theme from './theme/theme';
+import WhatsAppButton from './components/WhatsapChatBot/WhatsAppButton';
+import ChatAssistant from './components/WhatsapChatBot/ChatAssistant';
+import ChatWidget from './components/WhatsapChatBot/ChatWidget';
 
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <MainRoutes/>
+      <WhatsAppButton/>
+      <ChatAssistant/>
+      <ChatWidget/>
     </ThemeProvider>
     
   );
